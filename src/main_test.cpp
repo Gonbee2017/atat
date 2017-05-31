84ef75eb40b2d5521dfecb0eea83ccb350d28904
onamae <onamae1@outlook.jp>
2017-05-31 17:50:17 +0900

    fix

--- src/main_test.cpp
+++ src/main_test.cpp
@@ -1,6 +1,7 @@
-#include <CppUTest/CommandLineTestRunner.h>
+#include<CppUTest/CommandLineTestRunner.h>
 
-int main(int argc,char**argv) {
+int main(int argc,char**argv)
+{
     MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
     return RUN_ALL_TESTS(argc,argv);
 }
